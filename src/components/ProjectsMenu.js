265d5ef1eb10aa07798f44420fec10535689fda5
Pusaka Manggala <pusakamanggala@gmail.com>
2023-01-31 17:49:10 +0700

    add : Projects Menu

---
+++ src/components/ProjectsMenu.js
@@ -0,0 +1,39 @@
+import React from "react";
+import TwoForGame from "../img/24Game.jpg";
+import Workspot from "../img/Workspot.jpg";
+
+const ProjectsMenu = () => {
+  return (
+    <div className="pt-32">
+      <h1 className="text-4xl text-slate-900 font-poppins mb-0">Projects</h1>
+      <div className="flex py-20 justify-center ">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 lg:max-w-4xl w-full">
+          <div className="w-72 h-96 bg-slate-400 rounded-2xl mx-auto relative overflow-hidden">
+            {/* <div className="w-full h-full bg-black absolute opacity-0 hover:opacity-90 p-6">
+              <h1 className="text-white">
+                Arithmetical game in which the objective is to find a way to
+                manipulate four or six integers so that the end result is 24
+              </h1>
+            </div> */}
+            <h1 className="w-full  bg-slate-900 absolute bottom-0 p-3 text-white">
+              24 Game <br /> Mobile Game App
+            </h1>
+            <img src={TwoForGame} alt="" />
+          </div>
+          <div className="w-72 h-96 bg-slate-400 rounded-2xl mx-auto relative overflow-hidden">
+            <h1 className="w-full bg-slate-900 absolute bottom-0 p-3 text-white">
+              Workspot <br /> Job Requirements Website
+            </h1>
+            <img src={Workspot} alt="" />
+          </div>
+          <div className="w-72 h-96 bg-black rounded-2xl mx-auto"> </div>
+          <div className="w-72 h-96 bg-black rounded-2xl mx-auto"> </div>
+          <div className="w-72 h-96 bg-black rounded-2xl mx-auto"> </div>
+          <div className="w-72 h-96 bg-black rounded-2xl mx-auto"> </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsMenu;
