08ef3a5aaeeb828375454e9f8ce2a1fa789e8fa1
Pusaka Manggala <pusakamanggala@gmail.com>
2023-01-31 17:49:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import HeroSections from "./components/HeroSections";
+import ProjectsMenu from "./components/ProjectsMenu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-teal-500 ">
+      <Navbar ProjectID="Project" />
+      <section id="HeroSection">
+        <HeroSections />
+      </section>
+
+      <section id="Project">
+        <ProjectsMenu />
+      </section>
     </div>
   );
 }
