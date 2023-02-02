f89e9f1335ca15989f867091b866358514ceb462
Pusaka Manggala <pusakamanggala@gmail.com>
2023-02-02 16:10:02 +0700

    add : hover

--- src/components/ProjectsMenu.js
+++ src/components/ProjectsMenu.js
@@ -13,19 +13,19 @@ const ProjectsMenu = () => {
         </h1>
         <div className="flex py-20 justify-center ">
           <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:max-w-4xl w-full">
-            <div className="w-72 h-96 bg-slate-400 rounded-2xl mx-auto relative overflow-hidden shadow-md shadow-teal-900">
+            <div className="w-72 h-96 bg-slate-400 rounded-2xl mx-auto relative overflow-hidden shadow-md shadow-teal-900 hover:animate-pulse cursor-pointer">
               {/* <div className="w-full h-full bg-black absolute opacity-0 hover:opacity-90 p-6">
               <h1 className="text-white">
                 Arithmetical game in which the objective is to find a way to
                 manipulate four or six integers so that the end result is 24
               </h1>
             </div> */}
-              <h1 className="w-full  bg-slate-900 dark:bg-teal-300 absolute bottom-0 p-3 text-white font-semibold dark:text-slate-900 dark:shadow-teal-400">
+              <h1 className="w-full  bg-slate-900 dark:bg-teal-300 absolute bottom-0 p-3 text-white font-semibold dark:text-slate-900 dark:shadow-teal-400 ">
                 24 Game <br /> Mobile Game App
               </h1>
               <img src={TwoForGame} alt="" />
             </div>
-            <div className="w-72 h-96 bg-slate-400 rounded-2xl mx-auto relative overflow-hidden shadow-md shadow-teal-900">
+            <div className="w-72 h-96 bg-slate-400 rounded-2xl mx-auto relative overflow-hidden shadow-md shadow-teal-900 hover:animate-pulse cursor-pointer">
               <h1 className="w-full bg-slate-900  dark:bg-teal-300 absolute bottom-0 p-3 text-white font-semibold dark:text-slate-900 dark:shadow-teal-400">
                 WorkSpot <br /> Job Requirements Website
               </h1>
