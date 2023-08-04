849b82684205f7d4e49ba157fdd83ff2b640f6f7
Pusaka Manggala <pusakamanggala@gmail.com>
2023-08-04 19:44:30 +0700

    update : dark mode

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,36 +3,37 @@ import React, { useEffect } from "react";
 export const ThemeContext = React.createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [dark, setDark] = React.useState(false);
+  const [dark, setDark] = React.useState(() => {
+    // Check if a theme preference is stored in local storage
+    const storedDarkMode = localStorage.getItem("darkMode");
+
+    // Return the stored preference if it exists, or default to false
+    return storedDarkMode === "true";
+  });
+
+  const setDarkModePreference = (value) => {
+    // Set the theme preference in local storage
+    localStorage.setItem("darkMode", value.toString());
+    // Update the state
+    setDark(value);
+  };
 
   useEffect(() => {
-    // Check if user's browser theme preference is dark
-    const prefersDarkTheme = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-
     // Check if a theme preference is stored in local storage
     const storedDarkMode = localStorage.getItem("darkMode");
 
     // Set the theme based on the stored preference or user's browser theme
-    setDark(
-      storedDarkMode === "true" || (storedDarkMode === null && prefersDarkTheme)
-    );
-
-    // Clear the theme preference from local storage when the browser is closed
-    window.onbeforeunload = () => {
-      localStorage.removeItem("darkMode");
-    };
+    if (storedDarkMode === null) {
+      const prefersDarkTheme = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      setDarkModePreference(prefersDarkTheme);
+    }
   }, []);
 
   const ThemeToggle = () => {
-    setDark(!dark);
+    setDarkModePreference(!dark);
   };
-
-  useEffect(() => {
-    // Store the theme preference in local storage
-    localStorage.setItem("darkMode", dark.toString());
-  }, [dark]);
 
   return (
     <ThemeContext.Provider value={{ dark, ThemeToggle }}>
